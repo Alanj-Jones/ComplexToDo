58ba9d79a4cf1d113a3622f14559ab6d76a2724e
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-03-26 00:15:15 -0300

    List completed

--- ToDo/src/main/java/com/practice/App.java
+++ ToDo/src/main/java/com/practice/App.java
@@ -1,5 +1,7 @@
 package com.practice;
 
+import java.util.Scanner;
+
 public class App {
 
     private static void menu() {
@@ -11,8 +13,60 @@ public class App {
                             "0 - Exit\n" );
     }
     
+    public static int validateNumber(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException ex) {
+            System.out.println("Only numbers are allowed");
+        }
+        return -1;
+    }
+    
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        TaskLists tasks = new TaskLists();
+        loop: while (true) {
+            menu();
+            String input = scan.nextLine();
+            int validInput = validateNumber(input);
+            
+            switch(validInput) {
+                case 0:
+                    break loop;
+
+                case 1:
+                    tasks.printTasks();
+                    break;
+
+                case 2:
+                    if (tasks.getList().isEmpty()) {
+                        System.out.println("There are no tasks!");
+                        break ;
+                    }
+                    int option = validateNumber(scan.nextLine());
+                    tasks.removeTask(option);
+                    break;
+
+                case 3:
+                    String desc = scan.nextLine();
+                    tasks.addTask(desc);
+                    break;
+
+                case 4:
+                    if (tasks.getList().isEmpty()) {
+                        System.out.println("There are no tasks!");
+                        break ;
+                    }
+                    tasks.setToCompleted(validateNumber(scan.nextLine()));
+                    break;
+
+                default :
+                    break;
+            }
+        }
+
+        scan.close();
+
         
-        menu();
     }
 }
--- ToDo/src/main/java/com/practice/TaskLists.java
+++ ToDo/src/main/java/com/practice/TaskLists.java
@@ -17,6 +17,11 @@ public class TaskLists {
     }
     
     public void printTasks() {
+        if (myTasks.isEmpty()) {
+            System.out.println("There are no tasks!");
+            return ;
+        }
+
         for (Task t : myTasks) {
             System.out.println(myTasks.indexOf(t) + " | " + t.toString());
         }
@@ -27,6 +32,10 @@ public class TaskLists {
     }
 
     public void setToCompleted(int index) {
+        if (myTasks.isEmpty()) {
+            System.out.println("There are no tasks!");
+            return ;
+        }
         try {
             if (myTasks.get(index).isCompleted()) {
                 System.out.println("This task has already been marked as completed.");    
@@ -40,9 +49,8 @@ public class TaskLists {
 
 
     public void removeTask(int index) {
-        
-        myTasks.remove(index);
-        
+        index = validateIndex(index);
+        myTasks.remove(index);        
     }
 
     public int validateIndex(int index) {  
